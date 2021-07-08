1bbb347d85dfb6e86ac1510f15aaaa1088bb748b
Mrinmoy Nath <86364520+Mrin45@users.noreply.github.com>
2021-07-08 17:08:29 +0530

    Add files via upload

---
+++ src/index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body>
+        <?php
+        $conn = mysqli_connect("localhost","root","","banksystem","3307");
+if(!$conn){
+echo "not connected";
+}
+
+        ?>
+    </body>
+</html>
